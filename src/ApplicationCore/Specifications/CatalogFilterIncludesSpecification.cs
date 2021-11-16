909ba27e7d58e08a58c6776a30ca61f25dab24a1
Marcio Haruzo Otta <marcio.otta@cgu.gov.br>
2021-11-16 16:35:06 -0300

    fazendo specs

---
+++ src/ApplicationCore/Specifications/CatalogFilterIncludesSpecification.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Specifications
+{
+    public class CatalogFilterIncludeSpecification : Specification<CatalogItem>
+    {
+        public CatalogFilterIncludeSpecification(int? brandId, int? typeId)
+        {
+            Query.Where(i => (!brandId.HasValue || i.CatalogBrandId == brandId) &&
+                (!typeId.HasValue || i.CatalogTypeId == typeId));
+            Query.Include(c => c.CatalogBrand);
+            Query.Include(c=>c.CatalogType);
+        }
+    }
+}
--- src/Web/Pages/Admin/Index.cshtml.cs
+++ src/Web/Pages/Admin/Index.cshtml.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.eShopWeb.ApplicationCore.Constants;
 using Microsoft.eShopWeb.Web.Extensions;
 using Microsoft.eShopWeb.Web.Services;
 using Microsoft.eShopWeb.Web.ViewModels;
 using Microsoft.Extensions.Caching.Memory;
 using System.Threading.Tasks;
-
+using Microsoft.eShopWeb.Shared.Authorization;
+ 
 namespace Microsoft.eShopWeb.Web.Pages.Admin
 {
-    [Authorize(Roles = BlazorShared.Authorization.Constants.Roles.ADMINISTRATORS)]
+    [Authorize(Roles = Microsoft.eShopWeb.Shared.Authorization.Constants.Roles.ADMINISTRATORS)]
     public class IndexModel : PageModel
     {
         public IndexModel()
