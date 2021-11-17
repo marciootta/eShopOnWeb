a16791393f9f5854e91e195988f31d18b85150f5
Marcio Haruzo Otta <marcio.otta@cgu.gov.br>
2021-11-17 09:37:38 -0300

    aula 2 consultar e editar

--- src/Web/Pages/Admin/Index.cshtml.cs
+++ src/Web/Pages/Admin/Index.cshtml.cs
@@ -6,15 +6,33 @@ using Microsoft.eShopWeb.Web.ViewModels;
 using Microsoft.Extensions.Caching.Memory;
 using System.Threading.Tasks;
 using Microsoft.eShopWeb.Shared.Authorization;
- 
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Microsoft.eShopWeb.Web.Pages.Admin
 {
     [Authorize(Roles = Microsoft.eShopWeb.Shared.Authorization.Constants.Roles.ADMINISTRATORS)]
     public class IndexModel : PageModel
     {
-        public IndexModel()
+
+        CatalogItemViewModelService _catalogItemViewModelService;
+
+        public IndexModel(CatalogItemViewModelService catalogItemViewModelService)
         {
-           
+           _catalogItemViewModelService = catalogItemViewModelService;
+        }
+        public IEnumerable<CatalogAdminViewModel> Lista = new List<CatalogAdminViewModel>();
+
+        public async Task OnGet()
+        {
+            Lista = await _catalogItemViewModelService.GetAllCatalogItems();
+        }
+        public  IActionResult OnPostDelete(int id)
+        {
+             
+
+            return RedirectToPage();
         }
     }
 }
--- src/Web/Services/CatalogItemViewModelService.cs
+++ src/Web/Services/CatalogItemViewModelService.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.eShopWeb.Web.Pages.Admin;
 using Microsoft.eShopWeb.Web.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Web.Services
@@ -19,6 +23,37 @@ namespace Microsoft.eShopWeb.Web.Services
             var existingCatalogItem = await _catalogItemRepository.GetByIdAsync(viewModel.Id);
             existingCatalogItem.UpdateDetails(viewModel.Name, existingCatalogItem.Description, viewModel.Price);
             await _catalogItemRepository.UpdateAsync(existingCatalogItem);
+        }
+
+        public async Task<IEnumerable<CatalogAdminViewModel>> GetAllCatalogItems()
+        {
+            var catSpec = new CatalogWithBrandTypeSpecification(null, null);
+            var listaCatalog = await _catalogItemRepository.ListAsync(catSpec);
+
+            var listaCatalogAdminViewModel =
+                listaCatalog.Select(c => new CatalogAdminViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description,
+                    Price = c.Price,
+                    CatalogBrand = c.CatalogBrand.Brand,
+                    CatalogType = c.CatalogType.Type,
+                    PictureUri = c.PictureUri
+                });
+            return listaCatalogAdminViewModel;
+        }
+
+        public async Task<CatalogItemViewModel> GetCatalogItemViewModelById(int catalogId)
+        {
+            var item = await _catalogItemRepository.GetByIdAsync(catalogId);    
+            return new CatalogItemViewModel
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                PictureUri = item.PictureUri
+            };
         }
     }
 }
