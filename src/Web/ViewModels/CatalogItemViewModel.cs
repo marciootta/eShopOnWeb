6fb35e3e9b3af8b203a641e926e437cd0b65dc82
Marcio Haruzo Otta <marcio.otta@cgu.gov.br>
2021-11-17 16:29:25 -0300

    correcao validacoes

--- src/Web/ViewModels/CatalogItemViewModel.cs
+++ src/Web/ViewModels/CatalogItemViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Microsoft.eShopWeb.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.eShopWeb.Web.ViewModels
 {
     public class CatalogItemViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
         public string Name { get; set; }
         public string PictureUri { get; set; }
         public decimal Price { get; set; }
